9d3ed2618ab7f0a8614c5041b636c2addb8492ef
andy1729 <ujjawalanand1729@gmail.com>
2017-09-12 07:59:49 +0530

    Implemented onClick on Class level

--- app/src/main/java/com/mangoblogger/app/AboutFragment.java
+++ app/src/main/java/com/mangoblogger/app/AboutFragment.java
@@ -1,7 +1,6 @@
 package com.mangoblogger.app;
 
 import android.content.Intent;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,100 +8,91 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import java.util.Locale;
 
 /**
  * Created by karthikprasad on 7/29/17.
+ *  Fragment to show About view
  */
 
-public class AboutFragment extends Fragment{
+public class AboutFragment extends Fragment implements View.OnClickListener {
 
-    View view;
-    LinearLayout email, find_location;
-    ImageView facebook, twitter, youtube, linkedin, insta;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_about, container, false);
-
-        email = (LinearLayout) view.findViewById(R.id.send_email);
-        find_location = (LinearLayout) view.findViewById(R.id.find_location);
-        facebook = (ImageView) view.findViewById(R.id.facebook);
-        twitter = (ImageView) view.findViewById(R.id.twitter);
-        youtube = (ImageView) view.findViewById(R.id.youtube);
-        linkedin = (ImageView) view.findViewById(R.id.linkedin);
-        insta = (ImageView) view.findViewById(R.id.insta);
-
-        email.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_SEND);
-                getActivity().getIntent().setData(Uri.parse(("mailto: ")));
-                String[] to = {"contact@mangoblogger.com"};
-                intent.putExtra(Intent.EXTRA_EMAIL, to);
-                intent.putExtra(Intent.EXTRA_SUBJECT, "Mail from Mangoblogger App");
-                intent.setType("message/rfc822");
-                startActivity(Intent.createChooser(intent, "Send Email"));
-            }
-        });
-
-        find_location.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String uri = String.format(Locale.ENGLISH, "geo:%f,%f", 19.708079, 75.300217);
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-                getContext().startActivity(intent);
-            }
-        });
-
-        facebook.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse(
-                        "https://www.facebook.com/mangoblogger/"));
-                startActivity(browserIntent);
-            }
-        });
-
-        twitter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse(
-                        "https://twitter.com/mangoblogger"));
-                startActivity(browserIntent);
-            }
-        });
-
-        linkedin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse(
-                        "https://www.linkedin.com/company-beta/17919027"));
-                startActivity(browserIntent);
-            }
-        });
-
-        youtube.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse(
-                        "https://www.youtube.com/channel/UCCgxPOWNEqpcA60HnYg051w"));
-                startActivity(browserIntent);
-            }
-        });
-
-        insta.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse(
-                        "https://www.instagram.com/mangoblogger_analytics/"));
-                startActivity(browserIntent);
-            }
-        });
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        view.findViewById(R.id.send_email).setOnClickListener(this);
+        view.findViewById(R.id.send_whatsapp_msg).setOnClickListener(this);
+        view.findViewById(R.id.find_location).setOnClickListener(this);
+        view.findViewById(R.id.facebook).setOnClickListener(this);
+        view.findViewById(R.id.twitter).setOnClickListener(this);
+        view.findViewById(R.id.youtube).setOnClickListener(this);
+        view.findViewById(R.id.linkedin).setOnClickListener(this);
+        view.findViewById(R.id.insta).setOnClickListener(this);
         return view;
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.send_email:
+                startEmailIntent();
+                break;
+            case R.id.send_whatsapp_msg:
+                startWhatsappIntent();
+                break;
+            case R.id.find_location:
+                startLocationIntent();
+                break;
+            case R.id.facebook:
+                startBrowserIntent("https://www.facebook.com/mangoblogger/");
+                break;
+            case R.id.twitter:
+                startBrowserIntent("https://twitter.com/mangoblogger");
+                break;
+            case R.id.youtube:
+                startBrowserIntent("");
+                break;
+            case R.id.linkedin:
+                startBrowserIntent("https://www.linkedin.com/company-beta/17919027");
+                break;
+            case R.id.insta:
+                startBrowserIntent("https://www.youtube.com/channel/UCCgxPOWNEqpcA60HnYg051w");
+        }
+    }
+
+
+    private void startEmailIntent() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        getActivity().getIntent().setData(Uri.parse(("mailto: ")));
+        String[] to = {"contact@mangoblogger.com"};
+        intent.putExtra(Intent.EXTRA_EMAIL, to);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Mail from Mangoblogger App");
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "Send Email"));
+    }
+
+    private void startWhatsappIntent() {
+        String smsNumber = "3157518581";
+        Uri uri = Uri.parse("smsto:" + smsNumber);
+        Intent i = new Intent(Intent.ACTION_SENDTO, uri);
+        i.putExtra("sms_body", "Hi");
+        i.setPackage("com.whatsapp");
+        startActivity(i);
+    }
+
+    private void startLocationIntent() {
+        String uri = String.format(Locale.ENGLISH, "geo:%f,%f", 19.708079, 75.300217);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        getContext().startActivity(intent);
+    }
+
+    private void startBrowserIntent(String url) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse(
+                url));
+        startActivity(browserIntent);
+    }
+
+
 }
