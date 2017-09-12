a66fc657a2d67de9152d48e1f44a683932cb63b2
andy1729 <ujjawalanand1729@gmail.com>
2017-09-12 08:55:16 +0530

    created AppUtil class

---
+++ app/src/main/java/com/mangoblogger/app/AppUtils.java
@@ -0,0 +1,32 @@
+package com.mangoblogger.app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+/**
+ * Created by ujjawal on 12/9/17.
+ * Set of some useful Utils
+ */
+
+public class AppUtils {
+
+    public static boolean isOnWiFi(Context context) {
+        NetworkInfo activeNetwork = ((ConnectivityManager) context.getSystemService(
+                Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting() &&
+                activeNetwork.getType() == ConnectivityManager.TYPE_WIFI;
+    }
+
+    public static boolean hasConnection(Context context) {
+        NetworkInfo activeNetworkInfo = ((ConnectivityManager) context.getSystemService(
+                Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
+
+
+}
