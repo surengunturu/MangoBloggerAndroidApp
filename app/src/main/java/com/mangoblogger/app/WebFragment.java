c81c13e5e3de4196112aed91daa126ed1f4c3f25
andy1729 <ujjawalanand1729@gmail.com>
2017-09-14 16:42:04 +0530

    merged dev

--- app/src/main/java/com/mangoblogger/app/WebFragment.java
+++ app/src/main/java/com/mangoblogger/app/WebFragment.java
@@ -1,7 +1,6 @@
 package com.mangoblogger.app;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
