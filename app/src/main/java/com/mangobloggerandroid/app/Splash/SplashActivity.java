90735573fda5b48f9be464485b17c9386e3e857e
andy1729 <ujjawalanand1729@gmail.com>
2017-12-06 17:45:14 +0530

    removed previous splash screen

--- app/src/main/java/com/mangobloggerandroid/app/Splash/SplashActivity.java
+++ app/src/main/java/com/mangobloggerandroid/app/Splash/SplashActivity.java
@@ -1,19 +1,10 @@
 
 package com.mangobloggerandroid.app.Splash;
 
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-
 import com.google.firebase.analytics.FirebaseAnalytics;
-import com.mangobloggerandroid.app.R;
 import com.mangobloggerandroid.app.activity.WelcomeActivity;
 
 public class SplashActivity extends Activity {
