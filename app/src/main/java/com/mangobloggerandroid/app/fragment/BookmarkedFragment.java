c0dea4974e0094a215d9672b1d0ed24afc15f56b
Suren <surengunturu@berkeley.edu>
2018-07-27 13:41:00 -0700

    Updated products tab

--- app/src/main/java/com/mangobloggerandroid/app/fragment/BookmarkedFragment.java
+++ app/src/main/java/com/mangobloggerandroid/app/fragment/BookmarkedFragment.java
@@ -2,11 +2,19 @@ package com.mangobloggerandroid.app.fragment;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.mangobloggerandroid.app.R;
@@ -16,7 +24,7 @@ import com.mangobloggerandroid.app.interfaces.FragmentDataPasser;
  * Created by ujjawal on 11/10/17.
  */
 
-public class BookmarkedFragment extends Fragment {
+public class BookmarkedFragment extends Fragment implements View.OnClickListener {
     private FragmentDataPasser mDataPasser;
     public BookmarkedFragment() {
         // Required empty public constructor
@@ -38,6 +46,12 @@ public class BookmarkedFragment extends Fragment {
         if (getArguments() != null) {
             //args
         }
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.sendEmail).setOnClickListener(this);
     }
 
     @Override
@@ -74,4 +88,35 @@ public class BookmarkedFragment extends Fragment {
         analytics.setCurrentScreen(getActivity(), getClass().getSimpleName(), "Bookmark Screen");
 
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sendEmail:
+                startEmailIntent();
+                break;
+        }
+    }
+
+    private void startEmailIntent() {
+        Log.i("Send email", "");
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        if(intent.resolveActivity(getContext().getPackageManager()) != null) {
+            getActivity().getIntent().setData(Uri.parse(("mailto: ")));
+            String[] to = {""};
+            String[] cc = {""};
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_EMAIL, to);
+            intent.putExtra(Intent.EXTRA_CC, cc);
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Mangoblogger Employee Onboarding App Link");
+            String link_val = "https://chrome.google.com/webstore/detail/employee-onboarding-app/hmiefidoonaajllkjjhnhonkbcieincl?utm_source=app";
+            String body = "<a href=\"" + link_val + "\">" + link_val+ "</a>";
+            intent.putExtra(Intent.EXTRA_HTML_TEXT, Html.fromHtml(body));
+            /*intent.setType("message/rfc822");   TESTING*/
+            startActivity(Intent.createChooser(intent, "Send Email"));
+            Log.i("Finished sending email", "");
+        }
+    }
+
+
 }
